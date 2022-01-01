81efad94b8ef17396598cb999aa0df29bd487e5c
Shanawaj Mansuri <90132667+Shanawaj08@users.noreply.github.com>
2022-01-01 21:10:16 +0530

    Add files via upload

---
+++ discovery-service/src/main/java/com/sandy/discoveryservice/DiscoveryServiceApplication.java
@@ -0,0 +1,16 @@
+package com.sandy.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
+		System.out.println("!! DISCOVERY SERVICE is ALIVE !!");
+	}
+
+}
---
+++ discovery-service/src/test/java/com/sandy/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandy.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
